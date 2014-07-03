2ccba914041fa21fc5fe15e6905cf2221af800c5
Varun Rao <varunraob@gmail.com>
2014-07-03 16:09:09 -0500

    Updates to add agent on the master node

--- replace-slave-ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNTER=0
-for a in `grep "host_name" ambari-hosts.txt | awk -F':' '{print $(NF)}'`; do
-  let COUNTER=COUNTER+1
-  echo "Replacing slave$COUNTER with $a"
-  `sed -i "s/slave$COUNTER/$a/g" blueprint-cluster-definition.json`
-done

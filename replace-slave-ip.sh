4ba387cd59f1c9b9bf1a38e75281a973c629287a
Varun Rao <varunraob@gmail.com>
2014-07-03 14:06:09 -0500

    Updates to the script executed on the master

--- replace-slave-ip.sh
+++ replace-slave-ip.sh
@@ -3,5 +3,5 @@ COUNTER=0
 for a in `grep "host_name" ambari-hosts.txt | awk -F':' '{print $(NF)}'`; do
   let COUNTER=COUNTER+1
   echo "Replacing slave$COUNTER with $a"
-  `sed -i "s/slave$COUNTER/$a/g" blueprint-cluster-definition`
+  `sed -i "s/slave$COUNTER/$a/g" blueprint-cluster-definition.json`
 done

9eef908def6553607721a9584971f052e56a7bed
Varun Rao <varunraob@gmail.com>
2014-07-20 17:31:20 -0500

    Defect fix

--- replace-host-ip-for-cluster.sh
+++ replace-host-ip-for-cluster.sh
@@ -10,7 +10,7 @@ for slave_node_ip in `grep "host_name" ambari-hosts.txt | awk -F':' '{print $(NF
   	    	slave_node_ip_list="$slave_node_ip_list , \n"
        	fi
   	  echo "Replacing slave$COUNTER with $slave_node_ip"
-  	  slave_node_ip_list="$slave_node_ip_list \n\{\n\"fqdn\" : \"$slave_node_ip\"\n\}\n"
+  	  slave_node_ip_list="$slave_node_ip_list \n\{\n\"fqdn\" : $slave_node_ip\n\}\n"
   fi
 done
 `sed -i "s/slave_nodes/$slave_node_ip_list/;s/master_node/\"$master_node_ip\"/" blueprint-cluster-definition.json`
